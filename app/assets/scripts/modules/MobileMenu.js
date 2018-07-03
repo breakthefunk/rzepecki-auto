cb3a7a30e3897d45499c86f58ac26b1b5693a507
breakthefunk <maciej.k.krupa@gmail.com>
2018-07-03 19:30:07 +0200

    Started sidenav menu.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,18 +2,16 @@ import $ from 'jquery';
 
 class MobileMenu {
     constructor() {
-        this.siteHeader = $(".site-header")
+        this.siteHeader = $(".site-header");
         this.menuIcon = $(".site-header__menu-icon");
-        this.menuContent = $(".site-header__menu-content");
         this.events();
     }
     events() {
-        this.menuIcon.click(this.toggleTheMenu.bind(this));
-    }
+        this.menuIcon.click(console.log)
+    );}
     toggleTheMenu() {
-        this.menuContent.toggleClass("site-header__menu-content--is-visible")
-        this.siteHeader.toggleClass("site-header--is-expanded");
-        this.menuIcon.toggleClass("site-header__menu-icon--close-x");
+        this.toggleClass("sidenav--visible");
+        this.menuIcon.toggleClass("");
     }
 
 }
